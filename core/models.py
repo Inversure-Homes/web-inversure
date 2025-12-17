a8f79231ce76b3832c781f845fde081ddcc61a16
Miguel Angel Pérez <miguelangelperez@Mini-de-Miguel.lan>
2025-12-17 11:35:20 +0100

    Paso 3: formulario completo del simulador

--- core/models.py
+++ core/models.py
@@ -1,35 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
-
 
 class Operacion(models.Model):
-    usuario = models.ForeignKey(
-        User,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
-
-    fecha = models.DateTimeField(auto_now_add=True)
-
-    precio_compra = models.FloatField()
-    precio_venta = models.FloatField()
-    meses = models.FloatField()
-
-    inversion_total = models.FloatField()
-    beneficio_bruto = models.FloatField()
-
-    beneficio_inversure = models.FloatField()
-    beneficio_neto_inversure = models.FloatField()
-    beneficio_participes = models.FloatField()
-
-    rentabilidad_total = models.FloatField()
-    rentabilidad_anualizada = models.FloatField()
-
-    cumple_roi = models.BooleanField()
-
-    precio_breakeven = models.FloatField()
-    colchon_seguridad = models.FloatField()
+    nombre = models.CharField(max_length=255, blank=True)
+    precio_compra = models.FloatField(default=0)
+    precio_venta = models.FloatField(default=0)
+    inversion_total = models.FloatField(default=0)
+    beneficio_bruto = models.FloatField(default=0)
+    rentabilidad = models.FloatField(default=0)
+    creada = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Operación {self.id} - {self.fecha.strftime('%d/%m/%Y')}"
+        return self.nombre or f"Operacion {self.id}"
+
