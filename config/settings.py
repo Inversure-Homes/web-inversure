fafb7dd1dcb647cfb8b038aa3379fcf7b3916a8f
Miguel Angel Pérez <miguelangelperez@Mini-de-Miguel.lan>
2026-01-02 14:35:06 +0100

    Configure DATABASES using DATABASE_URL (PostgreSQL ready)

--- config/settings.py
+++ config/settings.py
@@ -89,14 +89,18 @@ TEMPLATES = [
 
 
 # =========================
-# BASE DE DATOS (DEMO)
+# BASE DE DATOS
 # =========================
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=False,
+    )
 }
 
 
