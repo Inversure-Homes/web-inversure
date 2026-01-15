09babfc36925e8c832ad48b9ffaf099b5cd5dfd6
Miguel Angel Pérez <miguelangelperez@Mini-de-Miguel.lan>
2026-01-15 22:49:44 +0100

    Redirect root to app when Wagtail has no live homepage

--- cms/views.py
+++ cms/views.py
@@ -1,9 +1,13 @@
+from django.http import Http404
 from django.shortcuts import redirect
 from wagtail.views import serve as wagtail_serve
 
 
 def root_or_app(request):
-    response = wagtail_serve(request, path="")
+    try:
+        response = wagtail_serve(request, path="")
+    except Http404:
+        return redirect("/app/")
     if response.status_code == 404:
         return redirect("/app/")
     return response
